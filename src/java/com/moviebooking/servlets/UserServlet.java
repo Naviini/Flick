826273e533b880e3454c0d34931b36938ea021c4
Navini <nvw369@gmail.com>
2024-12-15 23:50:45 +0530

    10th commit by NAVINI

--- src/java/com/moviebooking/servlets/UserServlet.java
+++ src/java/com/moviebooking/servlets/UserServlet.java
@@ -36,15 +36,19 @@ public class UserServlet extends HttpServlet {
         String name = request.getParameter("name");
         String email = request.getParameter("email");
         String password = request.getParameter("password");
-
+        String userType = request.getParameter("userType");
+        
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-            String sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+//            String sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+            String sql = "INSERT INTO users (name, email, password, user_type) VALUES (?, ?, ?, ?)";
+            
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1, name);
             ps.setString(2, email);
             ps.setString(3, password);
+             ps.setString(4, userType);
             ps.executeUpdate();
             conn.close();
             response.sendRedirect("login.html");
@@ -72,74 +76,39 @@ public class UserServlet extends HttpServlet {
             if (rs.next()) {
                 HttpSession session = request.getSession();
                 session.setAttribute("userId", rs.getInt("id"));
-                response.sendRedirect("home.jsp");
+//                response.sendRedirect("home.jsp");
+//            } else {
+//                response.getWriter().println("Invalid cr0edentials.Try again!");
+//                response.sendRedirect("login.html");
+             session.setAttribute("userType", rs.getString("user_type"));
+                session.setAttribute("userName", rs.getString("name"));
+                
+                // Redirect based on user type
+                String userType = rs.getString("user_type");
+                if ("admin".equals(userType)) {
+                    response.sendRedirect("admin/index.html");
+                } else {
+                    response.sendRedirect("home.jsp");
+                }
             } else {
-                response.getWriter().println("Invalid cr0edentials.Try again!");
-                response.sendRedirect("login.html");
+                response.sendRedirect("login.html?error=invalid");
             }
             conn.close();
         } catch (Exception e) {
             e.printStackTrace();
             response.getWriter().println("Error: " + e.getMessage());
         }
+    
+    
+    //login
+//    httpSession.setAttribute("current-user,user", user);
+//    
+//    if(user.getUserType)
+    //admin.jsp
+    //user.jsp
+    
+    
+    
     }
 }
     
-    
-//    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//        response.setContentType("text/html;charset=UTF-8");
-//        try (PrintWriter out = response.getWriter()) {
-//            /* TODO output your page here. You may use following sample code. */
-//            out.println("<!DOCTYPE html>");
-//            out.println("<html>");
-//            out.println("<head>");
-//            out.println("<title>Servlet UserServlet</title>");
-//            out.println("</head>");
-//            out.println("<body>");
-//            out.println("<h1>Servlet UserServlet at " + request.getContextPath() + "</h1>");
-//            out.println("</body>");
-//            out.println("</html>");
-//        }
-//    }
-//
-//    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-//    /**
-//     * Handles the HTTP <code>GET</code> method.
-//     *
-//     * @param request servlet request
-//     * @param response servlet response
-//     * @throws ServletException if a servlet-specific error occurs
-//     * @throws IOException if an I/O error occurs
-//     */
-//    @Override
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//        processRequest(request, response);
-//    }
-//
-//    /**
-//     * Handles the HTTP <code>POST</code> method.
-//     *
-//     * @param request servlet request
-//     * @param response servlet response
-//     * @throws ServletException if a servlet-specific error occurs
-//     * @throws IOException if an I/O error occurs
-//     */
-//    @Override
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//        processRequest(request, response);
-//    }
-//
-//    /**
-//     * Returns a short description of the servlet.
-//     *
-//     * @return a String containing servlet description
-//     */
-//    @Override
-//    public String getServletInfo() {
-//        return "Short description";
-//    }// </editor-fold>
-//
-//}
